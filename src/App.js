9c199d47d82cea157858c4557cfe621b0d840739
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-10-04 13:13:20 +0000

    added increment and decrement, state is in App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-import React, { Component } from 'react';
-import Counter from './Counter';
+import React from "react";
+import Counter from "./Counter";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state={
-      data : [
+    this.state = {
+      data: [
         { id: 1, value: 0 },
         { id: 2, value: 0 },
         { id: 3, value: 0 }
       ]
-    }
+    };
   }
 
-  changeValue = () =>{
-    console.log('clicked')
+  incrementCount = id => {
+    this.setState( prev => ({
+      ...prev,
+      data: prev.data.map(item => item.id === id ? ({...item, value: item.value + 1}) : item )
+    }))
+  }
+
+  decrementCount = id => {
+    this.setState( prev => ({
+      ...prev,
+      data: prev.data.map( item => item.id === id ? ({...item, value: item.value - 1}) : item)
+    }))
   }
   render() {
     return (
       <div>
-        {this.state.data.map(counter => ( 
-          <Counter 
-            key={counter.id} 
-            
-            value={counter.value} 
-            changeValue={this.changeValue}
-            />
+        {this.state.data.map((counter) => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            incrementCount={this.incrementCount}
+            decrementCount={this.decrementCount}
+            buttonId={counter.id}
+          />
         ))}
       </div>
     );
   }
 }
 
-export default App
+export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
+import React from "react";
 
-// Counter Component
-class Counter extends Component {
+class Counter extends React.Component {
+  handleIncrement = () => {
+    this.props.incrementCount(this.props.buttonId);
+    // console.log(this.props.buttonId);
+  };
+  handleDecrement = () => {
+    this.props.decrementCount(this.props.buttonId);
+    // console.log(this.props.buttonId);
+  };
+
   render() {
-    const { value, changeValue } = this.props;
+    const { value } = this.props;
+
     return (
       <div className="counter">
         <b>{value}</b>
         <div className="counter-controls">
-          <button className="button is-danger is-small" onClick={() => changeValue()}>-</button>
-          <button className="button is-success is-small" onClick={() => changeValue()}>+</button>
+          <button className="button is-danger is-small" onClick={this.handleDecrement} >-</button>
+          <button
+            className="button is-success is-small"
+            onClick={this.handleIncrement}
+          >
+            +
+          </button>
         </div>
       </div>
     );
   }
 }
-  
- 
-export default Counter;
+export default Counter; 
