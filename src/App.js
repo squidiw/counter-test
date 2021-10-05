31cc2d65ce40ce52ea7ace2552677fe661288f9c
Koranteng Anane <squidiw.squidiw@gmail.com>
2021-10-05 17:16:57 +0000

    added code to generate new counter onClick

--- src/App.js
+++ src/App.js
@@ -2,45 +2,59 @@ import React from "react";
 import Counter from "./Counter";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        { id: 1, value: 0 },
-        { id: 2, value: 0 },
-        { id: 3, value: 0 }
-      ]
-    };
-  }
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: [
+            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+         ],
+      };
+   }
 
-  incrementCount = id => {
-    this.setState( prev => ({
-      ...prev,
-      data: prev.data.map(item => item.id === id ? ({...item, value: item.value + 1}) : item )
-    }))
-  }
+   incrementCount = (id) => {
+      this.setState((prev) => ({
+         ...prev,
+         data: prev.data.map((item) =>
+            item.id === id ? { ...item, value: item.value + 1 } : item
+         ),
+      }));
+   };
 
-  decrementCount = id => {
-    this.setState( prev => ({
-      ...prev,
-      data: prev.data.map( item => item.id === id ? ({...item, value: item.value - 1}) : item)
-    }))
-  }
-  render() {
-    return (
-      <div>
-        {this.state.data.map((counter) => (
-          <Counter
-            key={counter.id}
-            value={counter.value}
-            incrementCount={this.incrementCount}
-            decrementCount={this.decrementCount}
-            buttonId={counter.id}
-          />
-        ))}
-      </div>
-    );
-  }
+   decrementCount = (id) => {
+      this.setState((prev) => ({
+         ...prev,
+         data: prev.data.map((item) =>
+            item.id === id ? { ...item, value: item.value - 1 } : item
+         ),
+      }));
+   };
+
+   addCounter = () => {
+      this.setState((prev) => ({
+        ...prev,
+         data: [...prev.data, {id: prev.data.length + 1, value: 0}],
+        //  data: [...prev.data, {id: prev.data.length + 1, value: 0})]
+      }));
+   };
+
+   render() {
+      return (
+         <div>
+            {this.state.data.map((counter) => (
+               <Counter
+                  key={counter.id}
+                  value={counter.value}
+                  incrementCount={this.incrementCount}
+                  decrementCount={this.decrementCount}
+                  buttonId={counter.id}
+               />
+            ))}
+            <button onClick={this.addCounter}>Add counter</button>
+         </div>
+      );
+   }
 }
 
 export default App;
